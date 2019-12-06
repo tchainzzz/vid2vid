c439593363314568e8091f176be3c06318b57a23
Trenton Chang <tchang97@stanford.edu>
2019-12-06 15:33:56 -0800

    Tested on single GPU

--- losses.py
+++ losses.py
@@ -1,3 +1,4 @@
+import os
 import torch.optim as optim
 import torch
 from torch.autograd import Variable
@@ -6,7 +7,9 @@ from models.vid2vid_model_G import Vid2VidModelG
 from options.train_options import TrainOptions 
 from data.data_loader import CreateDataLoader
 from models.models import create_model
-
+from util.visualizer import Visualizer
+import util.util as util
+from collections import OrderedDict
 
 
 """
@@ -22,7 +25,6 @@ def initialize(opt):
     opt.label_nc = 35
     opt.loadSize = 1024
     opt.n_scales_spatial = 3
-    opt.use_instance = True
     opt.fg = True
     opt.n_downsample_G = 2
     opt.use_real_img  = True 
@@ -31,7 +33,7 @@ def initialize(opt):
     opt.serial_batches = True  # no shuffle
     opt.no_flip = True  # no flip
     opt.dataset_mode = 'temporal'
-    opt.isTrain = False
+    opt.isTrain = False 
 
     print('------------ Options -------------')
     for k, v in sorted(vars(opt).items()):
@@ -42,8 +44,9 @@ def initialize(opt):
 """
     Using pytorch's loss function API, this calculates some loss metric.
 """
-def custom_loss(output, target):
-    pass
+def CustomLoss(output, target):
+    return output - target
+
 
 """
     The training loop.
@@ -56,7 +59,10 @@ def main():
     dataset = data_loader.load_data() # pass into dataroot loc. of our new segmentation maps
     model = create_model(opt)
 
+
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
+    for param in model.parameters():
+        param.requires_grad = True
 
     for epoch in range(opt.niter):
         epoch_loss = 0
@@ -64,18 +70,30 @@ def main():
             _, _, height, width = data['A'].size()
             A = Variable(data['A']).view(1, -1, 1, height, width) # folder A has the segmentation maps
             B = Variable(data['B']).view(1, -1, opt.output_nc, height, width) # folder B has the real images
-            inst = Variable(data['inst']).view(1, -1, 1, height, width) # instance maps
             
             # FORWARD PASS: GENERATE IMAGE
             optimizer.zero_grad()
-            output = model.inference(A, B, inst)
 
-            loss = custom_loss(output, B)
-            assert loss is not None, "Loss function cannot return NoneType!"
+            #fake, real = model.inference(A, B, None)
+            real_A, real_B, pool_map = model.encode_input(A, B, None)            
+            model.is_first_frame = not hasattr(model, 'fake_B_prev') or model.fake_B_prev is None
+            if model.is_first_frame:
+                model.fake_B_prev = model.generate_first_frame(real_A, real_B, pool_map)      
+            
+            real_A = model.build_pyr(real_A)            
+            model.fake_B_feat = model.flow_feat = model.fake_B_fg_feat = None            
+            for s in range(model.n_scales):
+                fake_B = model.generate_frame_infer(real_A[model.n_scales-1-s], s)
+            fake, real = fake_B, real_A[0][0, -1]
+
+
+            special_name = data['A_path'][0].split("/")[-1].split("_")[0]            
+            util.save_image(util.tensor2im(fake.data[0]), "fake_B_{}_epoch{}.{}".format(special_name, i, 'jpg'))
+            loss = CustomLoss(fake, data['B'][:,-1,...].cuda().detach())
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
-        print("loss, epoch {}/{}: {}".format(epoch + 1, opt.niter))
+        print("loss, epoch {}/{}: {}".format(epoch + 1, opt.niter, total_loss))
 
         # Save model
         save_filename = 'fewshot_net_G_{}.pth'.format(epoch)
