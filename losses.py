3a9838737211dddbf6ebd888e4a1643c43dec25b
Ubuntu <ubuntu@ip-172-31-57-235.us-west-2.compute.internal>
2019-12-08 09:21:15 +0000

    froze layers & adapted loss

--- losses.py
+++ losses.py
@@ -3,12 +3,14 @@ import torch.optim as optim
 import torch
 from torch.autograd import Variable
 import sys
+import models
 from models.vid2vid_model_G import Vid2VidModelG
 from options.train_options import TrainOptions 
 from data.data_loader import CreateDataLoader
 from models.models import create_model
 import util.util as util
 import time
+from pytorch_msssim import ms_ssim, MS_SSIM
 import ssim as pytorch_ssim
 
 """
@@ -73,18 +75,29 @@ def main():
     tG = opt.n_frames_G
 
     model,_, _ = create_model(opt)
-    print("---------Layers----------")
-    for name, child  in model.named_children():
-        print(name)
+
+    # freeze layers
+    for small_model in model.children():
+        for smaller_layer in small_model.children():
+            for even_smaller_layer in smaller_layer.children():
+                layers_to_freeze = sum(type(x) == models.networks.ResnetBlock for x in even_smaller_layer.children()) // 2
+                resnet_count = 0
+                set_trainable = True
+                for x in even_smaller_layer.children():
+                    if type(x) == models.networks.ResnetBlock: resnet_count += 1
+                    for param in x.parameters(): param.requires_grad = set_trainable
+                    if resnet_count > layers_to_freeze: set_trainable = False
+                    
+                
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
-    loss = pytorch_ssim.SSIM(window_size=11)
+    loss = pytorch_ssim.SSIM(window_size=11) 
     model = model.to(torch.device("cuda"))
     model.fake_B_prev = None
-    for param in model.parameters():
-        param.requires_grad = True
+
     
-    print()
+    best_loss = float('inf')
+    lambda_raw = 0.1
     for epoch in range(opt.niter):
         total_loss = 0
         start = time.time()
@@ -94,7 +107,7 @@ def main():
             B = data['B'].view(1, -1, opt.output_nc, height, width).requires_grad_(requires_grad=False) # folder B has the real images
             
             n_img = A.shape[1]
-            assert n_img > tG
+            save_seq = range(6) # save first 6 frames of each 
             for j in range(n_img - tG):
                 A_curr = A[:, j:j+tG, ...]
                 B_curr = B[:, j:j+tG,...]
@@ -102,10 +115,13 @@ def main():
                 optimizer.zero_grad()
                 outputs = model(A_curr, B_curr, None, model.fake_B_prev)
                 fake = outputs[0]
-                B_curr = B_curr.detach()
+                fake_raw = outputs[1]
+                real_B = outputs[5][:, 1:]
+                # util.save_image(util.tensor2im(real_B), "few_shot/fake_B.jpg") # temporary; to sanity check
+                real_B = real_B.detach()
 	
-                if j == 0 or epoch==opt.niter-1: util.save_image(util.tensor2im(fake.data[0]), "few_shot/results/fake_B_img{:04d}_epoch_{:04d}.{}".format(j, epoch, 'jpg'))
-                output = -loss(fake, B_curr[:, -1, ...].view(1, -1, opt.output_nc, height, width).cuda())
+                if j in save_seq or epoch==opt.niter-1: util.save_image(util.tensor2im(fake.data[0]), "few_shot/results/fake_B_img{:04d}_epoch_{:04d}.{}".format(j, epoch, 'jpg'))
+                output = - (loss(fake, real_B) + loss(fake_raw, real_B) * lambda_raw)
                 output.backward(retain_graph=False)
                 optimizer.step()
                 total_loss += output.item()
@@ -113,6 +129,9 @@ def main():
         print("\ntotal loss, epoch {}/{}: {} - took {:.4f}s".format(epoch + 1, opt.niter, total_loss, time.time() - start))
 
         # Save model
+        if total_loss < best_loss:
+            best_loss = total_loss
+            save_path = os.path.join("./few_shot/checkpoints", "latest_net_G0.pth") 
         save_filename = 'fewshot_net_G_{}.pth'.format(epoch)
         save_path = os.path.join("./few_shot/checkpoints", save_filename)
         torch.save(model.state_dict(), save_path)
